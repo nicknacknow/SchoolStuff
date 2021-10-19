28a2bf2e081ca81f74231371f5ce318d696a481e
nicholas <54144689+nicknacknow@users.noreply.github.com>
2021-10-19 22:41:57 +0100

    work pls

---
+++ ScrollMenu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScrollMenu
+{
+    class Program
+    {
+        static void ColorConsole(string msg, ConsoleColor col) 
+        {
+            ConsoleColor prev = Console.ForegroundColor;
+            Console.ForegroundColor = col;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = prev;
+        }
+
+        static void Callback(List<string> Options, int choice)
+        {
+            switch (choice)
+            {
+                case 0: case 1:
+                    ColorConsole("valid!!", ConsoleColor.Green);
+                    break;
+                case 2:
+                    ColorConsole("we don't live in 2016 anymore bro...", ConsoleColor.Red);
+                    break;
+            }
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            ScrollMenu<string> scrollMenu = new ScrollMenu<string>("select brand:", new List<string> { "gucci", "louis vuitton", "supreme" }, Callback);
+
+            scrollMenu.Start();
+        }
+    }
+}
---
+++ ScrollMenu/ScrollMenu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrollMenu
+{
+    class ScrollMenu<T>
+    {
+        public delegate void CallBack(List<T> Options, int choice);
+
+        public ScrollMenu(string msg, List<T> argOptions, CallBack argCallBack)
+        {
+            outputMsg = msg;
+
+            foreach (T str in argOptions)
+                options.Add(str);
+
+            callBack = argCallBack;
+        }
+
+        public void Start()
+        {
+            int choice;
+            Step(out choice);
+            callBack(options, choice);
+        }
+
+        private void Step(out int choice)
+        {
+            Console.WriteLine(outputMsg);
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (i == currentIndex)
+                {
+                    ConsoleColor prev = Console.ForegroundColor;
+                    Console.ForegroundColor = selectedColor;
+                    Console.WriteLine(options[i]);
+                    Console.ForegroundColor = prev;
+                }
+                else
+                    Console.WriteLine(options[i]);
+            }
+
+
+            ConsoleKey key = Console.ReadKey().Key;
+
+            switch (key)
+            {
+                case ConsoleKey.Enter: break;
+                case ConsoleKey.DownArrow:
+                    if (currentIndex < options.Count - 1) {  currentIndex++; }
+                    Console.Clear();
+                    Step(out choice);
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (currentIndex > 0) currentIndex--;
+                    Console.Clear();
+                    Step(out choice);
+                    break;
+                default:
+                    Console.Clear();
+                    Step(out choice);
+                    break;
+            }
+
+
+            choice = currentIndex;
+        }
+
+        //
+        // variables
+        //
+        
+        private string outputMsg = "";
+        private int currentIndex = 0;
+        private List<T> options = new List<T>();
+        private ConsoleColor selectedColor = ConsoleColor.DarkCyan;
+        private CallBack callBack;
+    }
+}
